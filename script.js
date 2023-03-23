09151e050bd67f5ba084de38054e1d73b5139676
Hash33m <118221605+Hash33m@users.noreply.github.com>
2023-03-22 23:39:44 -0500

    assets

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 // Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var passwordDetails = writePassword();
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  var passwordRequirements = {
+    allowLowerCase: window.confirm("Can lower case letters be used?"),
+    allowUpperCase: window.confirm("Can upper case letters be used?"),
+    allowNumbers: window.confirm("Can numbers be used?"),
+    allowSymbols: window.confirm("Can special characters and symbols be used?"),
+    passwordLenght: window.prompt("Please define password lenght."("Must be between 8 and 128 characters"))
+    console.log("Written as part of writePassword logic");
+    console.log(passwordRequirements);
+    return;
+
+  }
+
+  
 
   passwordText.value = password;
 
