3f25acd8bef3fa85888748f60a563c87299dcbd9
Hash33m <118221605+Hash33m@users.noreply.github.com>
2023-03-23 00:39:19 -0500

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // Assignment code here
 var generateBtn = document.querySelector("#generate");
+
 var passwordDetails = writePassword();
+
+function generatePassword(){
+console.log("Great job! You clicked the button!");
+
+  // 1. Prompt user for password criteria.
+  //  - Password lenght 8 < 128.
+  //  - Lowercase, uppercase, numbers, and special characters.
+  // 2. Validate the input.
+  // 3. Generate password based on criteria.
+  // 4. Display the password to the page.
+
+  return "Password will go here!";
+
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -17,12 +32,35 @@ function writePassword() {
     return;
 
   }
-
   
-
-  passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+/* Psuedo coding is done
+When it prompts user for passwordLength 
+Add logic to verify password length is between 8 and 128 characters 
+User is prompted to have:
+lowerCase
+lowercase= "abcdefghijklmnopqrstuvwxyz"
+
+UpperCase 
+upperCase= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+Numbers
+numbers= "012345678910"
+
+Special Characters and Symbols
+specialChar= "!@#$%&*()"
+
+Generate password 
+Use logic to make sure requirements are met and if not they should reloop
+
+
+
+
+
+
+
